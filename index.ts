565ea870a07be9313e911b196f690e20293b7ecf
Ayat-Talib <ayattalib890@gmail.com>
2024-04-04 17:01:06 +0500

    Tweked the text and add console line at end.

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+// Taking input from the user.
+const answere = await inquirer.prompt([{
+    name : "User_guessing_no",
+    type : "number",
+    message : "Please guessed the number between 1 to 10 :",
+}])
+// Generate an random number
+let random_number = Math.floor(Math.random() * 10)
+
+// comparing the number
+
+if (answere.User_guessing_no === random_number){
+    console.log("Congratzz!!, You Guessed Correct No!")
+} else{
+    console.log("OOPS, you guessed the wrong number.")
+}
